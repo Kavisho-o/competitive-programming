9daaee133829aad0c26ebafaad2e0cc6cded0791
Kavisho-o <kavisheleven011@gmail.com>
2026-03-24 23:23:50 +0530

    Implemented several math problems.

---
+++ maths/A_Park_Lighting.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+void solve() {
+
+    int n,m; cin>>n>>m;
+    
+    
+
+
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    return 0;
+}
